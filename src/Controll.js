dc4daa8adf2f904e64caa1a79054aaeedbd77f0d
Cheng,Chung-Yao <nark31408a@gmail.com>
2021-04-05 21:54:13 +0800

    UX

--- src/Controll.js
+++ src/Controll.js
@@ -1,36 +1,55 @@
 import { useEffect, useState } from 'react';
 import './Controll.css';
 function Controll({emitmessage}) {
+  const suits=["spades","hearts","clubs","diamonds"]
   const numbers=["aces","twos","threes","fours","fives","sixes","sevens","eights","nines","tens","jacks","queens","kings"]
-  const [time,setTime]=useState(1)
-  useEffect(()=>{
-    const timer=setTimeout(() => {
-      setTime(time=>time>=13?1:(time+1))
-    }, 1000);
-    // Clear timeout if the component is unmounted
-    return () => clearTimeout(timer);
+  const x2num=[0,1,2,5]
+  const [card,setcard]=useState({})
+  
+//   const [time,setTime]=useState(1)
+//   useEffect(()=>{
+//     const timer=setTimeout(() => {
+//       setTime(time=>time>=13?1:(time+1))
+//     }, 1000);
+//     // Clear timeout if the component is unmounted
+//     return () => clearTimeout(timer);
     
-  },)
+//   },)
+  const handleClick = (x)=>{
+    setcard((card)=>{
+        if(!card.suit){
+            return{suit:suits[x],number:0}
+        }
+        if(x===0){
+            emitmessage({suit:card.suit,number:numbers[(card.number-1)%13]})
+            return {}
+        }
+        else{
+            return{suit:card.suit,number:card.number+x2num[x]}
+        }
+    })
+    
+  }
   return (
     <>
       <div className="container">
         <div className="row">
           <div className="col" onClick={()=>{
-            emitmessage({suit:"spades",number:numbers[time-1]})
+            handleClick(0)
           }}/>
           <div className="col" onClick={()=>{
-            emitmessage({suit:"hearts",number:numbers[time-1]})
+            handleClick(1)
           }}/>
         </div>
         <div className="row">
           <div className="col" onClick={()=>{
-            emitmessage({suit:"clubs",number:numbers[time-1]})
+            handleClick(3)
           }}/>
           <div className="col" onClick={()=>{
-            emitmessage({suit:"diamonds",number:numbers[time-1]})
+            handleClick(2)
           }}/>
         </div>
-        <div className="time">{time}</div>
+        {/* <div className="time">{time}</div> */}
       </div>
     </>
   );
